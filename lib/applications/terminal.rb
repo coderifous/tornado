9ccde67fa262ef1735c2b5952149e22c87f32a9c
Jim Garvin <jim@thegarvin.com>
2009-01-09 22:27:17 -0500

    added handling of buggy windows bounds for Terminal app

--- lib/applications/terminal.rb
+++ lib/applications/terminal.rb
@@ -14,6 +14,30 @@ module Tornado::Applications::Terminal
     tab
   end
   
+  # For some reason (a bug), we have to add the window's height to both y-values in the coordinates
+  def position_top_left_corner_at(x,y)
+    b = current_bounds
+    app.windows[0].bounds.set([x, y + height, x + width, y + height + height])
+  end
+  
+  # same bug is addressed here
+  def resize(w, h)
+    b = current_bounds
+    puts b.inspect
+    w = absolutize_size(w, :width)
+    h = absolutize_size(h, :height)
+    app.windows[0].bounds.set([ b[0], b[1] + height, b[0] + w, b[1] + h + height ])
+  end
+  
+  def absolutize_size(size, width_or_height)
+    if size.to_s[-1].chr == "%"
+      OSX.send("desktop_#{width_or_height}") * (size.to_f / 100)
+    else
+      size + height
+    end
+  end
+  
+  
   class Tab
     attr_reader :tab
     
